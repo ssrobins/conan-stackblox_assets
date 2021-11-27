e974a4edd28f22dc666361ca07d4d426d9239ea3
Steve Robinson <ssrobins@gmail.com>
2021-11-27 15:56:14 -0800

    Initial package setup (#1)
    * Set up git lfs
    
    * Add assets
    
    * Remove README
    
    * Add conan package files
    
    * Add CI

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile
+
+class Conan(ConanFile):
+    name = "stackblox_assets"
+    version = "0.1.0"
+    description = "StackBlox assets"
+    license = "MIT"
+    url = f"https://github.com/ssrobins/conan-{name}"
+    revision_mode = "scm"
+    exports = "*"
+    build_policy = "missing"
+
+    def package(self):
+        self.copy("*", excludes=("conan.py", "conanbuildinfo.txt"))
