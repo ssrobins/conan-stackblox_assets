d287a5317b98108ba2594c5ed63217990ebc138e
Steve Robinson <ssrobins@gmail.com>
2022-01-15 11:24:43 -0800

    Use asset catalog for macOS icons (#2)
    * Remove old iOS icons
    
    * Add new iOS and macOS icons
    
    * Update Conan version

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class Conan(ConanFile):
     name = "stackblox_assets"
-    version = "1.1.0"
+    version = "2.0.0"
     description = "StackBlox assets"
     license = "MIT"
     url = f"https://github.com/ssrobins/conan-{name}"
